0ee963a6c0d1858127052a95886fbb35225d8663
Jungmi Park <55528172+Jummi10@users.noreply.github.com>
2021-09-09 18:15:00 +0900

    Time: 9 ms (66.73%), Space: 54.6 MB (41.40%) - LeetHub

--- shifting-letters/shifting-letters.java
+++ shifting-letters/shifting-letters.java
@@ -1,23 +1,15 @@
 class Solution {
     private static final int ALPHABET_SIZE = 26;
 
-    public String shiftingLetters(String str, int[] shifts) {
-        int length = str.length();
-
-        int[] shiftDp = new int[length + 1];
-        shiftDp[length] = 0;
-        for (int i = length - 1; i >= 0; i--) {
-            shiftDp[i] = (shiftDp[i + 1] + shifts[i]) % ALPHABET_SIZE;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            int ch = str.charAt(i) - 'a';
-            ch = (ch + shiftDp[i]) % ALPHABET_SIZE;
-            char shifted = (char)(ch + 97);
-            sb.append(shifted);
-        }
-
-        return sb.toString();
+    public String shiftingLetters(String s, int[] shifts) {
+        int n = shifts.length;
+        for (int i = n-2; i >= 0; i--)
+            shifts[i] += shifts[i+1]%26;
+        
+        char[] ret = new char[n];
+        for (int i = 0; i < n; i++)
+            ret[i] = (char) ((s.charAt(i)-'a'+shifts[i])%26+'a');
+        
+        return new String(ret);
     }
 }
